98aa436a7f08dbfd53233365e302f4306e1e0048
dtc66 <dtc66@cornell.edu>
2019-08-19 02:32:11 -0400

    reorganized to node file system

--- node_server.js
+++ node_server.js
@@ -12,11 +12,11 @@ http.createServer(function(req, res) {
 const express = require('express');
 const app = express();
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/index.html');
+    response.sendFile(__dirname + '/view/index.html');
 });
 
 // listen for requests :)
