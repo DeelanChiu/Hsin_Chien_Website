dce7b85ed499f24972d8d24af306d61f1d620282
dtc66 <dtc66@cornell.edu>
2019-08-22 05:09:53 -0400

    careers and contact page and excel database

--- node_server.js
+++ node_server.js
@@ -9,34 +9,36 @@ const readXlsx = require('read-excel-file/node');
 
 
 io.on('connection', function(socket) {
-    readXlsx('hcm_vessels.xlsx').then((rows) => {
-        // `rows` is an array of rows
-        // each row being an array of cells.
-        io.emit("vessel info", rows);
-    })
+    socket.on('query vessel', function() {
+        readXlsx('hcm_vessels.xlsx').then((rows) => {
+            // `rows` is an array of rows
+            // each row being an array of cells.
+            io.emit("vessel info", rows);
+        });
+    });
+    socket.on('query contact', function() {
+        readXlsx('hcm_contact.xlsx').then((rows) => {
+            // `rows` is an array of rows
+            // each row being an array of cells.
+            io.emit("contact info", rows);
+        });
+    });
+    socket.on('query career', function() {
+        readXlsx('hcm_careers.xlsx').then((rows) => {
+            // `rows` is an array of rows
+            // each row being an array of cells.
+            io.emit("career info", rows);
+        });
+    });
+
 });
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/view/index.html');
 });
-app.get('/index.html', function(request, response) {
-    response.sendFile(__dirname + '/view/index.html');
-});
-app.get('/about.html', function(request, response) {
-    response.sendFile(__dirname + '/view/about.html');
-});
-app.get('/organization.html', function(request, response) {
-    response.sendFile(__dirname + '/view/organization.html');
-});
-app.get('/vessels.html', function(request, response) {
-    response.sendFile(__dirname + '/view/vessels.html');
-});
-app.get('/products.html', function(request, response) {
-    response.sendFile(__dirname + '/view/products.html');
-});
-app.get('/store.html', function(request, response) {
-    response.sendFile(__dirname + '/view/store.html');
+app.get('/:page', function(request, response) {
+    response.sendFile(__dirname + '/view/' + request.params.page);
 });
 
 // listen for requests :)
